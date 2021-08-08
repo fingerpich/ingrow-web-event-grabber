4c474a567cd5a33fb71ec702c3e37eca247b42cf
fingerpich <zarei.bs@gmail.com>
2021-08-08 14:11:14 +0430

    updated get dom path

--- src/utils/get-dom-path.js
+++ src/utils/get-dom-path.js
@@ -5,25 +5,37 @@ export function getDomPath(el) {
   while ( el.parentNode != null ) {
     var sibCount = 0;
     var sibIndex = 0;
-    for ( var i = 0; i < el.parentNode.childNodes.length; i++ ) {
-      var sib = el.parentNode.childNodes[i];
-      if ( sib.nodeName == el.nodeName ) {
+    let hasDifferentID = !!el.id
+    let hasDifferentClassName = !!el.className
+    const children = el.parentNode.children
+    for ( var i = 0; i < children.length; i++ ) {
+      var sib = children[i];
+      if ( sib.nodeName === el.nodeName ) {
         if ( sib === el ) {
           sibIndex = sibCount;
+        } else {
+          if (hasDifferentClassName && sib.className === el.className) {
+            hasDifferentClassName = false
+          } else if (hasDifferentID && sib.id === el.id) {
+            hasDifferentID = false
+          }
         }
-        sibCount++;
       }
+      sibCount++;
     }
-    if ( el.hasAttribute && el.hasAttribute('id') && el.id != '' ) {
-      stack.unshift(el.nodeName.toLowerCase() + '#' + el.id);
+
+    if ( hasDifferentID ) {
+      stack.unshift('#' + el.id);
+    } else if(hasDifferentClassName) {
+      stack.unshift('.' + el.className.split(" ").join("."));
     } else if ( sibCount > 1 ) {
-      stack.unshift(el.nodeName.toLowerCase() + ':eq(' + sibIndex + ')');
+      stack.unshift(':nth-child(' + (sibIndex+1) + ')');
     } else {
       stack.unshift(el.nodeName.toLowerCase());
     }
     el = el.parentNode;
   }
   const stackEl = stack.slice(1) // removes the html element
-  const domPathString = stackEl && stackEl.length ? stackEl.join(">") : "";
+  const domPathString = stackEl && stackEl.length ? stackEl.join(" > ") : "";
   return domPathString
 }
