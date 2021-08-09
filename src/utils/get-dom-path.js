b5d8729941258fefb3a157a66b2b858d5025c70c
fingerpich <zarei.bs@gmail.com>
2021-08-09 18:03:52 +0430

    updated dom path

--- src/utils/get-dom-path.js
+++ src/utils/get-dom-path.js
@@ -6,7 +6,8 @@ export function getDomPath(el) {
     var sibCount = 0;
     var sibIndex = 0;
     let hasDifferentID = !!el.id
-    let hasDifferentClassName = !!el.className
+    let hasDifferentNodeName = true
+    let hasDifferentClassName = el.className && typeof(el.className) === "string"
     const children = el.parentNode.children
     for ( var i = 0; i < children.length; i++ ) {
       var sib = children[i];
@@ -18,6 +19,8 @@ export function getDomPath(el) {
             hasDifferentClassName = false
           } else if (hasDifferentID && sib.id === el.id) {
             hasDifferentID = false
+          } else if (hasDifferentNodeName && sib.nodeName === el.nodeName) {
+            hasDifferentNodeName = false
           }
         }
       }
@@ -28,6 +31,8 @@ export function getDomPath(el) {
       stack.unshift('#' + el.id);
     } else if(hasDifferentClassName) {
       stack.unshift('.' + el.className.split(" ").join("."));
+    } else if(hasDifferentNodeName) {
+      stack.unshift('.' + el.nodeName.toLowerCase());
     } else if ( sibCount > 1 ) {
       stack.unshift(':nth-child(' + (sibIndex+1) + ')');
     } else {
@@ -36,6 +41,6 @@ export function getDomPath(el) {
     el = el.parentNode;
   }
   const stackEl = stack.slice(1) // removes the html element
-  const domPathString = stackEl && stackEl.length ? stackEl.join(" > ") : "";
+  const domPathString = stackEl && stackEl.length ? stackEl.join(">") : "";
   return domPathString
 }
